80ac64b0328bad2e06819b0495d1731e279121f9
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-09-03 10:17:17 +0530

    Image URL corrected for teem

--- src/routes/utils/constructHTML.js
+++ src/routes/utils/constructHTML.js
@@ -7,13 +7,13 @@ function constructHTML(ogpData){
   return new Promise((resolve, reject) => {
     try{
       let projectData = ogpData.result[0].root;
-      if(projectData.image){
-        let imageURLarray = projectData.image.url.split('/');
-        let imageId = imageURLarray[imageURLarray.length - 1];
-        projectData.image = {
-          url: imageId
-        };
+      console.log(projectData);
+      if(!projectData.image){
+          projectData.image = {
+              url: 'https://app.teem.works/images/project_blue.svg'
+          };
       }
+      console.log(projectData);
       let HTML = mustache.render(fs.readFileSync(path.join(__dirname,'../../../resources/html/template.html')).toString(),projectData);
       resolve(HTML);
     }
