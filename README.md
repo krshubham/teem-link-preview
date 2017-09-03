1f2d399a8233ecbd0e523ddb0f57dd82c9aa3776
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-09-03 09:11:07 +0530

    added logging for the server

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # teem-link-preview
-The independent link preview server for the teem web application
+
+The ser 
--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,21 @@
 import express from 'express';
 import routes from '../routes';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import path from 'path';
+import fs from 'fs';
+require('dotenv').config();
 const config = require('../../config.json');
 
 
 const port = config.port;
 const server = express();
+
+
+//create a write stream for the logs in the append mode
+let accessLogStream = fs.createWriteStream(path.join(__dirname,'../../log/access.log'),{ flags: 'a'});
+server.use(morgan('combined', {stream: accessLogStream}));
+server.use(morgan('combined'));
 
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(bodyParser.json());
